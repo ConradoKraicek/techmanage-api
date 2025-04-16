5df4b9cebe5dd0b0bf63575bca2c2f64f6c8e625
ConradoKraicek <Conradoacardoso@gmail.com>
2025-04-16 15:59:34 -0300

    feat(repository): cria UserRepository com método existsByEmail

---
+++ src/main/java/com/techmanage/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.techmanage.repository;
+
+
+import com.techmanage.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+}
